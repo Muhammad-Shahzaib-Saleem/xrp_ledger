ae96fc0b66faede4419a52b9b3dcdcf7126fa04a
mAc BoOk pRo <dotenterprises@mAcs-MacBook-Pro.local>
2023-11-18 20:43:03 +0500

    Basic setups for xrpl-py

--- python/xrpl-python.py
+++ python/xrpl-python.py
@@ -1,18 +1,34 @@
 # Define the network client
 from xrpl.clients import JsonRpcClient
-from xrpl.wallet import generate_faucet_wallet
 JSON_RPC_URL = "https://s.altnet.rippletest.net:51234/"
 client = JsonRpcClient(JSON_RPC_URL)
 
+
 # Create a wallet using the testnet faucet:
 # https://xrpl.org/xrp-testnet-faucet.html
-
+from xrpl.wallet import generate_faucet_wallet
 test_wallet = generate_faucet_wallet(client, debug=True)
 
-print(test_wallet)
+# Create an account str from the wallet
+test_account = test_wallet.address
 
-# print output
+# Derive an x-address from the classic address:
+# https://xrpaddress.info/
+from xrpl.core import addresscodec
+test_xaddress = addresscodec.classic_address_to_xaddress(test_account, tag=12345, is_test_network=True)
+print("\nClassic address:\n\n", test_account)
+print("X-address:\n\n", test_xaddress)
 
-# public_key:: 022FA613294CD13FFEA759D0185007DBE763331910509EF8F1635B4F84FA08AEE3
-# private_key:: -HIDDEN-
-# classic_address: raaFKKmgf6CRZttTVABeTcsqzRQ51bNR6Q
+
+# Look up info about your account
+from xrpl.models.requests.account_info import AccountInfo
+acct_info = AccountInfo(
+    account=test_account,
+    ledger_index="validated",
+    strict=True,
+)
+response = client.request(acct_info)
+result = response.result
+print("response.status: ", response.status)
+import json
+print(json.dumps(result, indent=4, sort_keys=True))
